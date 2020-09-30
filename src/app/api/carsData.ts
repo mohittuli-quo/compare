39b74a5f849d1c0ea4e2128d21feffedb11602e5
mohittuli-quo <mohit.tuli@quovantis.com>
2020-09-30 09:59:16 +0530

    Updated UI and Json Data for car Listing page

--- src/app/api/carsData.ts
+++ src/app/api/carsData.ts
@@ -13,23 +13,28 @@ export class InMemCarsData implements InMemoryDbService {
             name: 'baleno',
             mileage: '25',
             description: 'Sigma Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
-            'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-              // 'Engine and Transmission': [
-              //   {'Engine Type': 'VVT Petrol Engine'},
-              //   {'Displacement (cc)': '1197'},
-              //   { 'Max Power (bhp@rpm)': '81.80bhp@6000rpm' }
-              // ],
-              'fuel-performance': [
+            img: 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
+            'engine-transmission': [
+              { label : 'Engine Type',  enginetype: 'F8D Petrol Engine'},
+              { label : 'Displacement (cc)', displacement: '796'},
+              { label : 'Max Power (bhp@rpm)', maxpower: '81.80bhp@6000rpm' },
+              { label : 'Max Torque (nm@rpm)', maxtorque: '69Nm@3500rpm' },
+            ],
+            
+            'fuel-performance': [
                 { label:'Fuel & Performance', fueltype: 'petrol'},
-                { label:'Fuel & Performance', mileagecity: '18'},
-                { label:'Fuel & Performance', mileage: '37' }
+                { label:'Mileage (City)', mileagecity: '18'},
+                { label:'Mileage(ARAI)', mileage: '37' },
+                { label: 'Fuel Tank Capacity (Litres)', fuelSize: '35' },
               ],
-              // 'Dimensions & Capacity': [
-              //   {'Length (mm)': '3995'},
-              //   {'Width (mm)': '1745'},
-              //   { 'Height (mm)': '1510' },
-              //   {'Ground Clearance Unladen (mm)': '170'}
-              // ],`
+              
+              'dimensions-capacity': [
+                { label: 'Length (mm)', length : '3995'},
+                { label: 'Width (mm)', width: '1745'},
+                { label: 'Height (mm)', height: '1510' },
+                { label: 'Ground Clearance Unladen (mm)',  groundClearance : '170'}
+              ],
+              
             varient: ['Tour', 'Swift Tour', 'Tour Dzire'],
           },
           {
@@ -40,22 +45,24 @@ export class InMemCarsData implements InMemoryDbService {
               'Swift Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
             charging: 'yes',
             'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-            // 'Engine and Transmission': [
-            //   {'Engine Type': 'VVT Petrol Engine'},
-            //   {'Displacement (cc)': '1197'},
-            //   { 'Max Power (bhp@rpm)': '81.80bhp@6000rpm' }
-            // ],
+            'engine-transmission': [
+              { label : 'Engine Type',  enginetype: 'F8D Petrol Engine'},
+              { label : 'Displacement (cc)', displacement: '796'},
+              { label : 'Max Power (bhp@rpm)', maxpower: '81.80bhp@6000rpm' },
+              { label : 'Max Torque (nm@rpm)', maxtorque: '69Nm@3500rpm' },
+            ],
             'fuel-performance': [
               { label :'Fuel & Performance',  fueltype : 'petrol'},
               { label :'Fuel & Performance',  mileageCity : '18'},
-              {  label :'Fuel & Performance',  mileage : '37' }
+              { label :'Fuel & Performance',  mileage : '37' },
+              { label: 'Fuel Tank Capacity (Litres)', fuelSize: '32' }
             ],
-            // 'Dimensions & Capacity': [
-            //   {'Length (mm)': '3995'},
-            //   {'Width (mm)': '1745'},
-            //   { 'Height (mm)': '1510' },
-            //   {'Ground Clearance Unladen (mm)': '170'}
-            // ],
+            'dimensions-capacity': [
+              { label: 'Length (mm)', length : '3995'},
+              { label: 'Width (mm)', width: '1745'},
+              { label: 'Height (mm)', height: '1510' },
+              { label: 'Ground Clearance Unladen (mm)',  groundClearance : '170'}
+            ],
             varient: ['petrol', 'Diesel', 'VZI'],
           },
           {
@@ -66,22 +73,24 @@ export class InMemCarsData implements InMemoryDbService {
               'alto Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
             charging: 'no',
             'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-            // 'Engine and Transmission': [
-            //   {'Engine Type': 'VVT Petrol Engine'},
-            //   {'Displacement (cc)': '1197'},
-            //   { 'Max Power (bhp@rpm)': '81.80bhp@6000rpm' }
-            // ],
+            'engine-transmission': [
+              { label : 'Engine Type',  enginetype: 'F8D Petrol Engine'},
+              { label : 'Displacement (cc)', displacement: '796'},
+              { label : 'Max Power (bhp@rpm)', maxpower: '81.80bhp@6000rpm' },
+              { label : 'Max Torque (nm@rpm)', maxtorque: '69Nm@3500rpm' },
+            ],
             'fuel-performance': [
               { label :'Fuel & Performance',  fueltype : 'petrol'},
               { label :'Fuel & Performance',  mileagecity : '18'},
-              { label :'Fuel & Performance',  mileage : '37' }
+              { label :'Fuel & Performance',  mileage : '37' },
+              { label: 'Fuel Tank Capacity (Litres)', fuelSize: '35' }
             ],
-            // 'Dimensions & Capacity': [
-            //   {'Length (mm)': '3995'},
-            //   {'Width (mm)': '1745'},
-            //   { 'Height (mm)': '1510' },
-            //   {'Ground Clearance Unladen (mm)': '170'}
-            // ],
+            'dimensions-capacity': [
+              { label: 'Length (mm)', length : '3995'},
+              { label: 'Width (mm)', width: '1745'},
+              { label: 'Height (mm)', height: '1510' },
+              { label: 'Ground Clearance Unladen (mm)',  groundClearance : '170'}
+            ],
             varient: ['VXI', 'VDI', 'VZI'],
           },
           {
@@ -92,75 +101,29 @@ export class InMemCarsData implements InMemoryDbService {
               'dzire Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
             charging: 'no',
             'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-            // 'Engine and Transmission': [
-            //   {'Engine Type': 'VVT Petrol Engine'},
-            //   {'Displacement (cc)': '1197'},
-            //   { 'Max Power (bhp@rpm)': '81.80bhp@6000rpm' }
-            // ],
+            'engine-transmission': [
+              { label : 'Engine Type',  enginetype: 'F8D Petrol Engine'},
+              { label : 'Displacement (cc)', displacement: '796'},
+              { label : 'Max Power (bhp@rpm)', maxpower: '81.80bhp@6000rpm' },
+              { label : 'Max Torque (nm@rpm)', maxtorque: '69Nm@3500rpm' },
+            ],
             'fuel-performance': [
               { label :'Fuel & Performance',  fueltype : 'petrol'},
               { label :'Fuel & Performance', mileagecity : '18'},
-              { label :'Fuel & Performance', mileage : '37' }
+              { label :'Fuel & Performance', mileage : '37' },
+              { label: 'Fuel Tank Capacity (Litres)', fuelSize: '28' }
             ],
-            // 'Dimensions & Capacity': [
-            //   {'Length (mm)': '3995'},
-            //   {'Width (mm)': '1745'},
-            //   { 'Height (mm)': '1510' },
-            //   {'Ground Clearance Unladen (mm)': '170'}
-            // ],
+            'dimensions-capacity': [
+              { label: 'Length (mm)', length : '3995'},
+              { label: 'Width (mm)', width: '1745'},
+              { label: 'Height (mm)', height: '1510' },
+              { label: 'Ground Clearance Unladen (mm)',  groundClearance : '170'}
+            ],
             varient: ['Tour', 'Swift Tour', 'Tour Dzire'],
           },
         ],
       },
-      // {
-      //   brandName: 'hyundai',
-      //     models: [
-      //       {
-      //         id: 200,
-      //         name: 'i10',
-      //         mileage: '18',
-      //         description: 'Sigma Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
-      //         engine: 'petrol',
-      //         charging: 'no',
-      //         'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-      //         varient: ['VXI', 'VDI', 'VZI'],
-      //       },
-      //       {
-      //         id: 201,
-      //         name: 'i20',
-      //         mileage: '20',
-      //         description:
-      //           'Swift Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
-      //         engine: 'petrol',
-      //         charging: 'yes',
-      //         'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-      //         varient: ['i20 VXI', 'i20 VDI', 'i20 VZI'],
-      //       },
-      //       {
-      //         id: 202,
-      //         name: 'creta',
-      //         mileage: '13',
-      //         description:
-      //           'alto Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
-      //         engine: 'petrol',
-      //         charging: 'no',
-      //         'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-      //         varient: ['cretaVXI', 'cretaVDI', 'cretaVZI'],
-      //       },
-      //       {
-      //         id: 203,
-      //         name: 'tucson',
-      //         mileage: '15',
-      //         description:
-      //           'dzire Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc Description Dummy Desc ',
-      //         engine: 'petrol',
-      //         charging: 'no',
-      //         'img': 'https://stimg.cardekho.com/images/carexteriorimages/360x240/Maruti/Maruti-Baleno/6780/1564491336282/front-left-side-47.jpg',
-      //         varient: ['VXI', 'VDI', 'VZI'],
-      //       },
-      //     ],
-      //   }
-     ]
+    ]
     return { carsList: carsListAPI };
   }
 }
--- src/app/compare-cars/compare-cars.component.ts
+++ src/app/compare-cars/compare-cars.component.ts
@@ -1,89 +1,106 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { CarsListService } from '../service/cars-list.service';
+import {
+    Component,
+    OnInit,
+    Input,
+    ViewChild
+} from '@angular/core';
+import {
+    CarsListService
+} from '../service/cars-list.service';
 
 
 @Component({
-selector: 'app-compare-cars',
-templateUrl: './compare-cars.component.html',
-styleUrls: ['./compare-cars.component.scss'],
+    selector: 'app-compare-cars',
+    templateUrl: './compare-cars.component.html',
+    styleUrls: ['./compare-cars.component.scss'],
 })
 export class CompareCarsComponent implements OnInit {
-carsList = [];
-varientMatch=[];
+    carsList = [];
+    varientMatch = [];
 
-selectedCars:any[] = [];
-showTab:Boolean = false;
-selectcar: Boolean = false;
+    selectedCars: any[] = [];
+    showTab: Boolean = false;
+    selectcar: Boolean = false;
+    filteredData: any[] = [];
+    tabLists: string[] = ['Carname', 'Carmodel'];
+    selectTabLists = '';
+    getCarModelArr = [{
+            model: 'select Model/Varient',
+            varient: [{}],
+            img: '',
+            removedVarients: [],
+            selectedVarient: []
+        },
+        {
+            model: 'select Model/Varient',
+            varient: [{}],
+            img: '',
+            removedVarients: [],
+            selectedVarient: []
+        },
+        {
+            model: 'select Model/Varient',
+            varient: [{}],
+            img: '',
+            removedVarients: [],
+            selectedVarient: []
+        },
+    ];
 
-tabLists: string[] = ['Carname', 'Carmodel'];
-selectTabLists = '';
-getCarModelArr = [
-  { model: 'select Model/Varient', varient: ['select Model/Varient'], img:'' },
-  { model: 'select Model/Varient', varient: ['select Model/Varient'], img:''},
-  { model: 'select Model/Varient', varient: ['select Model/Varient'], img:''},
-];
-
-constructor(private carsListService: CarsListService) {}
+    constructor(private carsListService: CarsListService) {}
 
 
 
-  ngOnInit() {
-    this.getCarsListFromAPI();
+    ngOnInit() {
+        this.getCarsListFromAPI();
     }
-   
-  getCarsListFromAPI() {
-  this.carsListService.getCarsList().subscribe(res => {
-  this.carsList = res;
-  })
-  }
+
+    getCarsListFromAPI() {
+        this.carsListService.getCarsList().subscribe(res => {
+            this.carsList = res;
+        })
+    }
 
 
-selectCar(index) {
-this.showTab = index;
-this.selectTabLists = this.tabLists[0];
-}
+    selectCar(index) {
+        this.showTab = index;
+        this.selectTabLists = this.tabLists[0];
+    }
 
-getCarsName(carModel, index) {
+    getCarsName(carModel, index) {
 
-// const isExist = this.selectedCars.findIndex(c=>c.name==carModel.name);
-// if(isExist!=-1){
-// this.selectedCars = this.selectedCars.filter(x => x.name == carModel.name)
-// return;
-// }
-// else{ 
-// this.getCarModelArr[index] = carModel;
-// this.selectedCars[index] = this.getCarModelArr[index];
-// console.log(this.selectedCars); 
-// } 
-this.getCarModelArr[index] = carModel;
-this.selectedCars[index] = this.getCarModelArr[index];
-// console.log(this.selectedCars); 
+        this.getCarModelArr[index] = carModel;
+        this.selectedCars[index] = this.getCarModelArr[index];
 
-this.getCarModelArr[index].model = carModel.name;
-this.getCarModelArr[index].img = carModel.img; 
+        this.getCarModelArr[index].model = carModel.name;
+        this.getCarModelArr[index].img = carModel.img;
 
-if (this.getCarModelArr[index].model) {
-this.selectTabLists = this.tabLists[1];
-}
-}
+        if (this.getCarModelArr[index].model) {
+            this.selectTabLists = this.tabLists[1];
+        }
+    }
 
-getCarsVarient(carVarient, index) {
-this.getCarModelArr[index].varient[index] = carVarient;
-this.getCarModelArr[index] = this.getCarModelArr[index];
-// console.log(this.getCarModelArr);
-this.showTab = false;
-}
+ 
+    getCarsVarient(carVarient, index) {
+        // this.getCarModelArr[index].varient.slice(1, carVarient);
+        this.getCarModelArr[index].selectedVarient = carVarient;
 
-filteredData:any[]=[];
+        // this.getCarModelArr[index] = this.getCarModelArr[index];
+        // console.log(this.getCarModelArr[index].varient);
+        // console.log(this.getCarModelArr[index].selectedVarient);
+        console.log(this.getCarModelArr);
+ 
 
-compareCars(){
-this.filteredData = this.selectedCars;
-console.log(this.filteredData);
-if(this.filteredData.length<=1){
-alert("Please select at least 2 cars");
-}else{
-this.filteredData = this.selectedCars;
-}
 
-}
+        this.showTab = false;
+    }
+
+    compareCars() {
+        this.filteredData = this.selectedCars;
+        // console.log(this.filteredData);
+        if (this.filteredData.length <= 1) {
+            alert("Please select at least 2 cars");
+        }
+
+    }
 }
